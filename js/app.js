2ae6e9631611de081f33e1c81b17c3bc53834b8b
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-09-14 18:50:54 +1000

    now the game is initialized successfully

--- js/app.js
+++ js/app.js
@@ -24,13 +24,28 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
 
+};
+
+Player.prototype.update = function() {
+
+};
+
+Player.prototype.render = function() {
+
+};
+
+Player.prototype.handleInput = function() {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+
 // Place the player object in a variable called player
-
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
